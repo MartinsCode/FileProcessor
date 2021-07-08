e850c4ecdf0cfb789bcd4b506860593a767f9ad5
Martin Brunzel <martin.brunzel@gmx.de>
2021-07-08 14:37:27 +0200

    add class to write to carbon/whisper

---
+++ lib/carbon.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'socket'
+require 'time'
+
+##
+# Class Carbon connects to a carbon database and sends your measurement
+class Carbon
+  ##
+  # initialize new CarbonConnection
+  def initialize(socket = TCPSocket.new)
+    @socket = socket
+    @host = 'localhost'
+    @port = 2003
+  end
+
+  ##
+  # send a message via socket
+  def send(topic, value, timestamp)
+    message = Carbon.format_message(topic, value, timestamp)
+    send_socket(message)
+  end
+
+  ##
+  # handle sending on tcp layer
+  def send_socket(message)
+    puts "Opening #{@host}, port #{@port}"
+    @socket.open(@host, @port)
+    @socket.print message
+    @socket.close
+  end
+
+  ##
+  # formats a message to a single string which then can be sent to carbon in plain text format
+  # convert timestamp to unix epoch format on the way
+  def self.format_message(topic, value, timestamp)
+    "#{topic} #{value} #{format_time(timestamp)}"
+  end
+
+  ##
+  # convert timestamp to unix epoch time
+  def self.format_time(timestamp)
+    Time.parse(timestamp).to_i.to_s
+  rescue ArgumentError
+    # seems to be already unix epoch time, so leave it alone
+    timestamp
+  end
+end
---
+++ spec/carbon_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'carbon'
+
+RSpec.describe Carbon do
+  context 'in general' do
+    it 'format_message should return a message in carbon format' do
+      expect(Carbon.format_message('/cpu/load', '0.08', '2021-07-07 15:31')).to eq '/cpu/load 0.08 1625664660'
+      expect(Carbon.format_time('2021-07-07 15:31')).to eq '1625664660'
+      expect(Carbon.format_time('1625664660')).to eq '1625664660'
+      expect(Carbon.format_time('2021-07-07T13:31:00Z')).to eq '1625664660'
+      expect(Carbon.format_time('2021-07-21T13:31:00Z')).to eq '1626874260'
+    end
+
+    it 'sends the right message to the right tcp-socket' do
+      mock = double('TCPSocket')
+      expect(mock).to receive(:open).with('localhost', 2003)
+      expect(mock).to receive(:print).with('/test/local 12 1611238860')
+      expect(mock).to receive(:close)
+
+      carb = Carbon.new(mock)
+      carb.send('/test/local', 12, '2021-01-21 15:21')
+    end
+  end
+end
